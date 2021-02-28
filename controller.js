2692a1c4a47cc2c6c59d6ac0f8acd208e3936dd3
wes <wesboyt@amazon.com>
2021-02-28 12:24:00 -0800

    swapped to post

--- controller.js
+++ controller.js
@@ -70,7 +70,7 @@ function post(url, data) {
             console.log(xhr.response);
         }
     }
-    xhr.open("GET", url, true);
+    xhr.open("POST", url, true);
     xhr.setRequestHeader('Content-Type', 'application/json');
     xhr.send(JSON.stringify({
         value: data
