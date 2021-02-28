e4eeca2609ce12dbd4033477f554eb3278587824
wes <wesboyt@amazon.com>
2021-02-28 11:10:09 -0800

    nvm

--- controller.js
+++ controller.js
@@ -72,7 +72,6 @@ function post(url, data) {
     }
     xhr.open("POST", url, true);
     xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.setRequestHeader('Access-Control-Allow-Origin', 'https://exploits.gg/');
     xhr.send(JSON.stringify({
         value: data
     }));
